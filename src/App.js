6b627ca0bf8900e7257dedeb40c6a0104d8c98c3
Winfred Huang <huang.winfred21@gmail.com>
2021-03-17 16:09:38 -0700

    Render progress tracker with styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.div`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
+`
+
+const Title = styled.h1`
+  font-family: sans-serif;
+  color: #b19cd9;
+`
+
+const WeekdayTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  border-right: solid 3px lightgrey;
+`
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Title>Progress Tracker</Title>
+        <div>
+          <Section>
+            <WeekdayTitle>M</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>T</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>W</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>T</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>F</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>S</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>S</WeekdayTitle>
+          </Section>
+        </div>
+      </>
     );
   }
 }
