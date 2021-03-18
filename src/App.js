072914eed3731e55bd0ed4d8a40226527d34b144
Winfred Huang <huang.winfred21@gmail.com>
2021-03-17 18:18:37 -0700

    Create Section component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
 import React from 'react';
 import styled from 'styled-components';
-
-const Section = styled.div`
-  background-color: #ffffff;
-  border: solid 3px grey;
-  margin: 4px;
-  width: 300px;
-  height: 50px;
-  border-radius: 10px;
-  display: flex;
-  justify-content: left;
-  font-size: 40px;
-  color: #ff6961;
-`
+import Section from './components/Section';
 
 const Title = styled.h1`
   font-family: sans-serif;
   color: #b19cd9;
-`
-
-const WeekdayTitle = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 50px;
-  border-right: solid 3px lightgrey;
 `
 
 class App extends React.Component {
@@ -33,27 +14,13 @@ class App extends React.Component {
       <>
         <Title>Progress Tracker</Title>
         <div>
-          <Section>
-            <WeekdayTitle>M</WeekdayTitle>
-          </Section>
-          <Section>
-            <WeekdayTitle>T</WeekdayTitle>
-          </Section>
-          <Section>
-            <WeekdayTitle>W</WeekdayTitle>
-          </Section>
-          <Section>
-            <WeekdayTitle>T</WeekdayTitle>
-          </Section>
-          <Section>
-            <WeekdayTitle>F</WeekdayTitle>
-          </Section>
-          <Section>
-            <WeekdayTitle>S</WeekdayTitle>
-          </Section>
-          <Section>
-            <WeekdayTitle>S</WeekdayTitle>
-          </Section>
+          <Section day="M" />
+          <Section day="T" />
+          <Section day="W" />
+          <Section day="T" />
+          <Section day="F" />
+          <Section day="S" />
+          <Section day="S" />
         </div>
       </>
     );
